f18a369006c41605382e702cbff7fbe0e36b999c
VinayakUshakola <vinayak.ushakola@gmail.com>
2022-09-03 21:52:01 +0530

    Intial Commit

---
+++ BLProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BLProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello People");
+            Console.ReadKey();
+        }
+    }
+}
